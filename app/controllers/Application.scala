84488f0b229c68bd953457f6a29e2f0a5f4baad3
Eric Hanson <j.eric.hanson@gmail.com>
2014-01-23 12:51:07 -0500

    added simple proxy

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,12 +1,28 @@
 package controllers
 
-import play.api._
 import play.api.mvc._
+import play.api.libs.ws.WS
+import scala.concurrent.ExecutionContext.Implicits.global
 
 object Application extends Controller {
-  
+
   def index = Action {
     Ok(views.html.index("Your new application is ready!!!"))
   }
-  
+
+  def priceHistory(ticker: String) = Action {
+
+    val url = s"http://ichart.finance.yahoo.com/table.csv?s=$ticker"
+
+    Async {
+      WS.url(url).get().map {
+        response =>
+          response.status match {
+            case 200 => Ok(response.body)
+            case _ => NotFound
+          }
+      }
+    }
+  }
+
 }
