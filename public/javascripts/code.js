48b8a87c91dc7a87dbb3d7ce89e4c20dfd622871
Eric Hanson <j.eric.hanson@gmail.com>
2014-01-23 19:32:29 -0500

    comparison working with location

--- public/javascripts/code.js
+++ public/javascripts/code.js
@@ -1,22 +1,49 @@
 google.load('visualization', '1', {'packages': ['annotatedtimeline']});
 
 $(document).ready(function () {
+
+    var defaultBase = "VFINX";
+    var defaultVariable = "BOGIX";
+
+    var base = "";
+    var variable = "";
+
+
+    //read tickers from HASH
+    var tickers = window.location.hash.substr(1).split('/');
+    if (tickers.length == 2) {
+        base = tickers[1];
+        variable = tickers[0];
+    } else {
+        base = "VFINX";
+        variable = "BOGIX";
+    }
+
+    $("#base").val(base);
+    $("#compare").val(variable);
+
+    function setHash() {
+        window.location.hash = variable + "/" + base;
+
+    }
+
+
     $("#loadStuff").click(function (e) {
         e.preventDefault();
 
-        var baseTicker = $("#base").val();
-        var compareTicker = $("#compare").val();
+        base = $("#base").val();
+        variable = $("#compare").val();
 
-        console.log(baseTicker + " vs " + compareTicker);
+        console.log(base + " vs " + variable);
 
-        var result = jQuery.ajax("/compare/" + baseTicker + "/" + compareTicker);
+        var result = jQuery.ajax("/compare/" + base + "/" + variable);
 
 
         result.then(function (x) {
 
             var data = new google.visualization.DataTable();
             data.addColumn('date', 'Date');
-            data.addColumn('number', baseTicker + " vs " + compareTicker);
+            data.addColumn('number', variable + " vs " + base);
 
             var inputRows = x.split('\n');
             var outputRows = [];
@@ -28,7 +55,7 @@ $(document).ready(function () {
                 var fields = inputRows[i].split(',');
                 if (fields.length == 2) {
                     var date = new Date(fields[0]);
-                    var value = Number(fields[1]) -1;
+                    var value = Number(fields[1]) - 1;
 
                     if (value > max) max = value;
                     if (value < min) min = value;
@@ -43,8 +70,11 @@ $(document).ready(function () {
             var chart = new google.visualization.AnnotatedTimeLine(document.getElementById('chart_div'));
             chart.draw(data, {displayAnnotations: true, min: min, max: max});
 
+            setHash();
         });
 
         return false;
     });
+
+    $("#loadStuff").click();
 });
